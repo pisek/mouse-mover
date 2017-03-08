b70c129481dec05cadb3e6c4426221636eaf9c97
Pisek <pisekfm@o2.pl>
2017-03-08 13:42:22 +0100

    mover

--- src/main/java/ga/servu/mousemover/Mover.java
+++ src/main/java/ga/servu/mousemover/Mover.java
@@ -1,5 +1,7 @@
 package ga.servu.mousemover;
 
+import java.awt.*;
+
 /**
  * Created by Pisek on 08.03.2017.
  */
@@ -7,7 +9,18 @@ public class Mover {
 
     public static void main(String[] args) {
 
+        int moveBy = 1;
 
+        while (true) {
+            try {
+                Point l = MouseInfo.getPointerInfo().getLocation();
+                Robot r = new Robot();
+                r.mouseMove(l.x + moveBy, l.y);
+                moveBy = -moveBy;
+                Thread.sleep(60000);
+            } catch (Exception e) {
+            }
+        }
 
     }
 
